ba00cc6e297a2dc00ee56ab871e5ef476507c1d9
Brian Glaws <glaws.brian@gmail.com>
2022-02-23 19:59:20 -0500

    added randomNums function to return set of randomly ordered integers between 1 and 15

--- Assignment2/src/Board.js
+++ Assignment2/src/Board.js
@@ -1,33 +1,44 @@
-// let X = [4];
-// let Y = [4];
 let Board = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
+    [, , , ],
+    [, , , ],
+    [, , , ],
+    [, , , ]
 ]
-
-//tis function is a test to see if printBoard is working properly
-function fillBoard () {
-    let num = 1;
+//FILL BOARD IS NOT WORKING PROPERLY
+//this function is a test to see if printBoard is working properly
+function fillBoard (nums) {
+    //convert set to array to iterate through it
+    const arr = Array.from(nums)
+    let index = 0
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
-            Board[i][j] = num;
-            num++;
+            Board[i][j] = arr[index++];
         }
     }
 }
 
 //returns set of numbers between 1 and 15 in random order
 function randomNums () {
-    let nums = new Set([1])
-    // fix this loop!
-    while (!nums < 15) {
+    let nums = new Set([])
+    let count = 0
+
+    // THIS WORKS BUT IS INEFFICIENT
+    while (nums.size < 15) {
         let rand = Math.floor(Math.random() * 15) + 1
         nums.add(rand)
-        console.log(rand)
+        console.log(count++)
     }
     return nums
+}
+
+// This function is a more efficient version of randomNums. Come back to this if everything else is working.
+function recursiveRandomNums (nums, rand) {
+    if (nums.size >= 15) {
+        return
+    }
+    if (nums.has(rand)) {
+        return 
+    }
 }
 
 function printBoard () {
@@ -48,18 +59,8 @@ function printBoard () {
 }
 
 
-console.log(randomNums())
+fillBoard(randomNums)
 
+console.log(Board)
 
-// function printNums () {
-//     let nums = randomNums()
-//     for (let item of nums) console.log(item)
-// }
-
-// printNums()
-
-//console.log(Board)
-
-// fillBoard()
-
-// printBoard()
+// console.log(randomNums())
