d2805b18a9bf2e3634fff20c3d9767af59ab2844
Bglaws <glaws.brian@gmail.com>
2022-03-04 23:47:27 -0500

    added get solution and equals functions to BFS.js

--- Assignment2/src/BFS.js
+++ Assignment2/src/BFS.js
@@ -1,3 +1,9 @@
+
+const solution = false
+
+// for array comparisons
+const equals = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2);
+
 function Queue () {
 
 }
@@ -14,7 +20,7 @@ function getEmptyTile (Board) {
             // console.log("j = ", j)
             if(Board[i][j] === undefined) {
                 // save the address of the empty tile
-                let address = [[i],[j]]
+                let address = [3,3]
                 return address
             }
         }
@@ -31,47 +37,40 @@ function getEmptyTile (Board) {
 export function getMove (Board) {
     const address = getEmptyTile(Board)
 
-    let x, y = 0
+    // console.log(address)
 
-    console.log(address)
+    console.log(Board)
 
-    // This aint working :/
-    // need to rewrite the different cases
-    // need to check if address is = to address on the board
-    switch (address) {
-        // corners
-        // top left
-        case x === 0 && y === 0:
-            return ['N', 'W']
-        // top right 
-        case x === 3 && y === 0:
-            return ['N', 'E']
-        // bottom left
-        case x === 0 && y === 3:
-            return ['S', 'W']
-        // bottom right
-        //address[1] == 3 && address[0] == 3
-        case address === [[3],[3]]:
-            return ['S', 'E']
+    // corners
+    // top left
+    if (equals(address, [0,0])) return ['N', 'W']
+    // top right
+    else if (equals(address, [0,3])) return ['N', 'E']
+    // bottom left
+    else if (equals(address, [3,0])) return ['S', 'W']
+    // bottom right
+    else if (equals(address, [3,3])) return ['S', 'E']
 
-        // sides
-        // top
-        case x === 1 && y === 0 || x === 2 && y === 0:
-            return ['N', 'E', 'W']
-        // bottom
-        case x === 1 && y === 3 || x === 2 && y === 3:
-            return ['S', 'E', 'W']
-        // left side
-        case x === 0 && y === 1 || x === 0 && y === 2:
-            return ['N', 'S', 'W']
-        // right side
-        case x === 3 && y === 1 || x === 3 && y === 2:
-            return ['N', 'S', 'E']
+    // sides
+    // top
+    else if (equals(address, [0,1]) && equals(address, [0,2])) return ['N', 'E', 'W']
+    // bottom
+    else if (equals(address, [3,1]) && equals(address, [3,2])) return ['S', 'E', 'W']
+    // left side
+    else if (equals(address, [1,0]) && equals(address, [2,0])) return ['N', 'S', 'W']
+    // right side 
+    else if (equals(address, [1,3]) && equals(address, [2,3])) return ['N', 'S', 'E']
 
-        // inner square
-        default:
-            return ['N', 'S', 'E', 'W']
-    }    
+    // inner circle
+    // upper left
+    else if (equals(address, [1,1])) return ['N','S','E','W']
+    // upper right
+    else if (equals(address, [1,2])) return ['N','S','E','W']
+    // bottom left
+    else if (equals(address, [2,1])) return ['N','S','E','W']
+    // bottom right
+    else if (equals(address, [2,2])) return ['N','S','E','W']
+
 }
 
 /**getNode recieves list of possible moves from getMoves,
@@ -79,5 +78,17 @@ export function getMove (Board) {
  */
 export function getNode (Board) {
     let moves = getMove(Board)
-    console.log(moves)
+    // console.log(moves)
+
+    // this for loop performs the moves, and return the subsequent nodes
+    for (let i = 0; i < moves.length; i++) {
+
+    }
+}
+
+function getSolution () {
+    // while solution is not found, keep making new nodes and checking them
+    for (let i = 0; !solution; i ++) {
+        
+    }    
 }
