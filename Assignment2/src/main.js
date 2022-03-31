15c569a9af907ddb3daa3166d7eb383c1a7fb97c
Bglaws <glaws.brian@gmail.com>
2022-03-30 23:20:53 -0400

    added a switch statement to main. main now accepts command line arguments for which search algorithm to use.

--- Assignment2/src/main.js
+++ Assignment2/src/main.js
@@ -4,20 +4,46 @@ import { writeFile } from "fs";
 import { Queue } from "./Queue.js";
 import { DFSSolution } from "./DFS.js";
 
+const args = process.argv
 let puzzle = new Node()
-puzzle.getNewBoard()
+
+switch(args[2]) {
+    
+    case 'bfs':
+        puzzle.getNewBoard()
+        BFSSolution(puzzle)
+        break
+    case 'dfs':
+        // puzzle.getNewBoard()
+        // DFS takes too long to solve with random puzzle
+        puzzle.board = [
+            [1,2,3,4],
+            [5,6,7,8],
+            [9,10,11,12],
+            [13,14,'X',15]
+        ]
+        DFSSolution(puzzle)
+    case 'ids':
+        // IDSSolution(puzzle)
+    case 'a*':
+        aStarSolution
+    default:
+        console.log("invalid argument")
+}
+
+
 
 // BFS solution
 // BFSSolution(puzzle)
 
 // DFS is much simpler, since it is less efficient
-let DFSPuzzle = new Node()
-DFSPuzzle.board = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,'X',15]
-]
+// let DFSPuzzle = new Node()
+// DFSPuzzle.board = [
+//     [1,2,3,4],
+//     [5,6,7,8],
+//     [9,10,11,12],
+//     [13,14,'X',15]
+// ]
 
-// DFS solution
-DFSSolution(DFSPuzzle)
+// // DFS solution
+// DFSSolution(DFSPuzzle)
