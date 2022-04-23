ae80739e1e2c464dfdecdfb7dffb33ee45aeb80e
Bglaws <glaws.brian@gmail.com>
2022-04-23 01:54:59 -0400

    deleted old folder

--- Assignment2/README.md
+++
@@ -1,8 +0,0 @@
-
-## Finished randomNums function. Need to fix fillBoard.
-
-### TODO
-BFS.js
-DFS.js
-IDS.js 
-main.js
--- Assignment2/src/Solution.js
+++
@@ -1,7 +0,0 @@
-// Solution to the puzzle
-export const SOLUTION = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 'X']
-]
